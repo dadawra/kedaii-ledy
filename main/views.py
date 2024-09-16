b2d24072d7b94abd945f90c874862f9e83118fcf
Dara <darazapri@gmail.com>
2024-09-16 22:34:59 +0700

    menambahkan forms untuk input product

--- main/views.py
+++ main/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect   
+from main.forms import ProductEntryForm
+from main.models import ProductEntry
 
-# Create your views here.
 def show_main(request):
+    product_entries = ProductEntry.objects.all()
+
     context = {
-        'npm' : '2306165906',
-        'name': 'Dara Zakya Apriani',
-        'class': 'PBP B'
+        'name': 'Pak Bepe',
+        'class': 'PBP D',
+        'npm': '2306123456',
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
